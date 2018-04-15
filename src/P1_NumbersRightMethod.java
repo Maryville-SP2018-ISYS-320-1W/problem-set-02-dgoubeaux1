e63e9780a0525d24c20251efec41cd7ea2a61b0e
Destiny Goubeaux <dgoubeaux1@live.maryville.edu>
2018-04-15 02:30:26 -0500

    changes

--- src/P1_NumbersRightMethod.java
+++ src/P1_NumbersRightMethod.java
@@ -8,7 +8,7 @@ public class P1_NumbersRightMethod {
 
 	public static void main(String[] args) {
 
-		public void printReversedTriangle(int 7)
+		public void drawNumbersRight(7)
 		{
 		    for(int i=0; i<=7; i++)
 		    {
